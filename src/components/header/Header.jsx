cbed80ab5056233a2285b638454cb1b6e58aaf10
sergio <sergiousl@yahoo.com>
2019-02-28 13:44:16 +0100

    smoth scrolling added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 import SvgText from "../IntroSvg";
+
 import ArrowDown from "../../assets/images/arrow-down.svg";
 import Profile from "../../assets/images/Sergio.png";
 import $ from "jquery";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 class Header extends Component {
   componentDidMount() {
-    $(function () {
+    $(function() {
       $(".intro").addClass("go");
 
-      $(".reload").click(function () {
+      $(".reload").click(function() {
         $(".intro")
           .removeClass("go")
           .delay(200)
-          .queue(function (next) {
+          .queue(function(next) {
             $(".intro").addClass("go");
             next();
           });
       });
     });
   }
+
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -27,21 +34,31 @@ class Header extends Component {
           class="d-flex justify-content-between flex-wrap flex-md-row intro"
           id="top"
         >
-          <div className="intro-polygon">
-
-          </div>
+          <div className="intro-polygon" />
 
           <div class="text-ani">
             <SvgText />
           </div>
-          <div>
-            <img
-              className="down-arrow"
-              src={ArrowDown}
-              alt=""
-            />
-          </div>
+          <Link
+                activeClass="active"
+                to="section1"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+            <img className="down-arrow" src={ArrowDown} alt="" />
+          </Link>
         </div>
+
+
+        <section
+        style={{ height: "100vw", width: "300px", backgroundColor: "red" }}
+          title="section1"
+
+          dark={true}
+          id="section1"
+        />
 
       </React.Fragment>
     );
